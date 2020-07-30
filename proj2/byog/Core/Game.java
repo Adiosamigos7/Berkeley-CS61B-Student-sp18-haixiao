359013589ac1b65780277ce3fe055387af118c43
haixiao <yuhxthu@gmail.com>
2020-07-29 22:56:46 -0700

    proj2 ph1 ver3

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -1,11 +1,11 @@
 package byog.Core;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import java.awt.*;
-import java.util.*;
+import java.awt.Rectangle;
+import java.awt.Point;
+import java.util.Random;
+import java.util.ArrayList;
 
-import byog.TileEngine.TERenderer;
+
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
 
@@ -13,12 +13,11 @@ import byog.TileEngine.Tileset;
 
 
 public class Game {
-    TERenderer ter = new TERenderer();
     /* Feel free to change the width and height. */
     public static final int WIDTH = 80;
     public static final int HEIGHT = 30;
     public static final long SEED = 4;
-    public static final TETile[][] tiles = new TETile[WIDTH][HEIGHT];;
+    public static final TETile[][] TILES = new TETile[WIDTH][HEIGHT];;
     private static Random RANDOM;
     /** the method of world generation comes from:
      * @source: https://gamedevelopment.tutsplus.com/tutorials/
@@ -33,23 +32,20 @@ public class Game {
     /** private class of binary spacing partitioning. */
 
     private class Leaf {
-        public String name;
         /** position and size of the leaf */
-        public int x, y, width, height;
+        private int x, y, width, height;
         /** left and right child of the leaf. */
-        public Leaf leftChild, rightChild;
+        private Leaf leftChild, rightChild;
         /** room inside the leaf */
-        /** TODO: make sure how to use Rectangle. */
-        public Rectangle room;
+        private Rectangle room;
         /** hallways to connect this leaf to other leafs */
-        public ArrayList<Rectangle> halls;
+        private ArrayList<Rectangle> halls;
         /** constructor */
-        public Leaf(int xpos, int ypos, int w, int h, String s) {
+        public Leaf(int xpos, int ypos, int w, int h) {
             this.x = xpos;
             this.y = ypos;
             this.width = w;
             this.height = h;
-            this.name = s;
         }
 
         /** Split a leaf. If successfully split, return true. */
@@ -83,11 +79,11 @@ public class Game {
             int split = RANDOM.nextInt(max - minleafsize);
             split += minleafsize;
             if (splitH) {
-                this.leftChild = new Leaf(this.x, this.y, width, split, this.name.concat("-hl"));
-                this.rightChild = new Leaf(this.x, this.y + split, width, height - split, this.name.concat("-hr"));
+                this.leftChild = new Leaf(this.x, this.y, width, split);
+                this.rightChild = new Leaf(this.x, this.y + split, width, height - split);
             } else {
-                this.leftChild = new Leaf(this.x, this.y, split, height, this.name.concat("-vl"));
-                this.rightChild = new Leaf(this.x + split, this.y, width - split, height, this.name.concat("-vr"));
+                this.leftChild = new Leaf(this.x, this.y, split, height);
+                this.rightChild = new Leaf(this.x + split, this.y, width - split, height);
             }
             return true;
         }
@@ -101,14 +97,17 @@ public class Game {
                 if (this.rightChild != null) {
                     this.rightChild.createRooms();
                 }
-                /** if there are both left and right children in this Leaf, create a hallway between  */
+                /** if there are both left and right children in this Leaf,
+                 * create a hallway between leafs. */
                 if (leftChild != null && rightChild != null) {
                     createHall(leftChild.getRoom(), rightChild.getRoom());
                 }
             } else {
                 /** leaf does not have child. Ready to make a room. */
-                Point roomSize = new Point(RANDOM.nextInt(this.width - minleafsize + 1) + minleafsize / 2,
-                                        RANDOM.nextInt(this.height - minleafsize + 1) + minleafsize / 2);
+                Point roomSize = new Point(RANDOM.nextInt(this.width - minleafsize + 1)
+                        + minleafsize / 2,
+                                        RANDOM.nextInt(this.height - minleafsize + 1)
+                                                + minleafsize / 2);
                 Point roomPos = new Point(RANDOM.nextInt(width - roomSize.x - 2) + 1,
                                             RANDOM.nextInt(height - roomSize.y - 2) + 1);
                 this.room = new Rectangle(x + roomPos.x, y + roomPos.y, roomSize.x, roomSize.y);
@@ -119,13 +118,13 @@ public class Game {
 
         /** method to convert room to tiles */
         public void drawroom() {
-            for(int tx = this.room.x; tx <= this.room.x + this.room.width; tx++) {
-                for(int ty = this.room.y; ty <= this.room.y + this.room.height; ty++) {
-                    if (tx == this.room.x || tx == this.room.x + this.room.width ||
-                        ty == this.room.y || ty == this.room.y + this.room.height) {
-                        tiles[tx][ty] = Tileset.WALL;
+            for (int tx = this.room.x; tx <= this.room.x + this.room.width; tx++) {
+                for (int ty = this.room.y; ty <= this.room.y + this.room.height; ty++) {
+                    if (tx == this.room.x || tx == this.room.x + this.room.width
+                            || ty == this.room.y || ty == this.room.y + this.room.height) {
+                        TILES[tx][ty] = Tileset.WALL;
                     } else {
-                        tiles[tx][ty] = Tileset.FLOOR;
+                        TILES[tx][ty] = Tileset.FLOOR;
                     }
                 }
             }
@@ -161,7 +160,8 @@ public class Game {
             }
         }
 
-        /** method to create one or two two-tile-thick rectangles (hallway) that connects two rooms together.
+        /** method to create one or two two-tile-thick rectangles (hallway)
+         *  that connects two rooms together.
          * For now, just draw straight line or right angle line to connect.
          * Can be improved later.*/
         public void createHall(Rectangle l, Rectangle r) {
@@ -169,13 +169,13 @@ public class Game {
             int hallwidth = 1;
             int p1x = l.x + 1, p1y = l.y + 1, p2x = r.x + 1, p2y = r.y + 1;
             if (l.width > 3) {
-                p1x += RandomUtils.uniform(RANDOM,1 , l.width - 2);
+                p1x += RandomUtils.uniform(RANDOM, 1, l.width - 2);
             }
             if (l.height > 3) {
                 p1y += RandomUtils.uniform(RANDOM, 1, l.height - 2);
             }
             if (r.width > 3) {
-                p2x += RandomUtils.uniform(RANDOM,1 , r.width - 2);
+                p2x += RandomUtils.uniform(RANDOM, 1, r.width - 2);
             }
             if (r.height > 3) {
                 p2y += RandomUtils.uniform(RANDOM, 1, r.height - 2);
@@ -246,63 +246,54 @@ public class Game {
         }
         /** method to convert hall to tiles */
         public void drawhall(Rectangle h, String s) {
-            if (s == "h") {
+            if (s.equals("h")) {
                 int ty = h.y;
                 for (int tx = h.x; tx < h.x + h.width; tx++) {
-                    tiles[tx][ty] = Tileset.FLOOR;
+                    TILES[tx][ty] = Tileset.FLOOR;
                 }
                 for (int tx = h.x - 1; tx <= h.x + h.width; tx++) {
-                    if (tiles[tx][ty - 1] == Tileset.NOTHING) {
-                        tiles[tx][ty - 1] = Tileset.WALL;
+                    if (TILES[tx][ty - 1] == Tileset.NOTHING) {
+                        TILES[tx][ty - 1] = Tileset.WALL;
                     }
-                    if (tiles[tx][ty + 1] == Tileset.NOTHING) {
-                        tiles[tx][ty + 1] = Tileset.WALL;
+                    if (TILES[tx][ty + 1] == Tileset.NOTHING) {
+                        TILES[tx][ty + 1] = Tileset.WALL;
                     }
                 }
-            } else if (s == "v") {
+            } else if (s.equals("v")) {
                 int tx = h.x;
                 for (int ty = h.y; ty < h.y + h.height; ty++) {
-                    tiles[tx][ty] = Tileset.FLOOR;
+                    TILES[tx][ty] = Tileset.FLOOR;
                 }
                 for (int ty = h.y - 1; ty <= h.y + h.height; ty++) {
-                    if (tiles[tx - 1][ty] == Tileset.NOTHING) {
-                        tiles[tx - 1][ty] = Tileset.WALL;
+                    if (TILES[tx - 1][ty] == Tileset.NOTHING) {
+                        TILES[tx - 1][ty] = Tileset.WALL;
                     }
-                    if (tiles[tx + 1][ty] == Tileset.NOTHING) {
-                        tiles[tx + 1][ty] = Tileset.WALL;
+                    if (TILES[tx + 1][ty] == Tileset.NOTHING) {
+                        TILES[tx + 1][ty] = Tileset.WALL;
                     }
                 }
             }
-        }
-
-
-        /** for tests - print leafs. */
-        public void printleaf() {
-            System.out.println("Leaf " + this.name + ": XY Position: (" + x + ", " + y +
-                                "), Width: " + width + ", Height: " + height + ".");
         }
     }
 
     /**initialize world tiles */
     private void tileInit() {
-        for (int i = 0; i < tiles.length; i++) {
-            for (int j = 0; j < tiles[0].length; j++) {
-                tiles[i][j] = Tileset.NOTHING;
+        for (int i = 0; i < TILES.length; i += 1) {
+            for (int j = 0; j < TILES[0].length; j += 1) {
+                TILES[i][j] = Tileset.NOTHING;
             }
         }
     }
     /** Construct the world frame */
-    public TETile[][] World(long seed) {
+    public TETile[][] world(long seed) {
         RANDOM = new Random(seed);
         world = new ArrayList<Leaf>();
-        /** Create a root of all leafs. */
-        Leaf root = new Leaf(0,0, WIDTH, HEIGHT,"root");
+        Leaf root = new Leaf(0, 0, WIDTH, HEIGHT);
         world.add(root);
         tileInit();
         boolean issplit = true;
         /** loop through every leaf in the ArrayList over and over again,
-         * until no more leafs.
-         */
+         * until no more leafs. */
         while (issplit) {
             issplit = false;
             for (int i = 0; i < world.size(); i++) {
@@ -319,7 +310,7 @@ public class Game {
             }
         }
         root.createRooms();
-        return tiles;
+        return TILES;
     }
 
 
@@ -344,16 +335,10 @@ public class Game {
      * @return the 2D TETile[][] representing the state of the world
      */
     public TETile[][] playWithInputString(String input) {
-        TERenderer ter = new TERenderer();
-        ter.initialize(WIDTH, HEIGHT);
         long seed = Long.parseLong(input.substring(1, input.length() - 2));
-        // TODO: Fill out this method to run the game using the input passed in,
-        // and return a 2D tile representation of the world that would have been
-        // drawn if the same inputs had been given to playWithKeyboard().
-
         TETile[][] finalWorldFrame = null;
         Game g = new Game();
-        finalWorldFrame = g.World(seed);
+        finalWorldFrame = g.world(seed);
         return finalWorldFrame;
     }
 
